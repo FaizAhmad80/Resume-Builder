e9050e219b1b47616bdceb8b95f53f748d49065b
Faiz Ahmad <35074955-faizahmadug21@users.noreply.replit.com>
2024-07-06 17:56:14 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import Header from "./components/Header/Header";
+import Body from "./components/Body/Body";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+    </div>
+  );
+}
